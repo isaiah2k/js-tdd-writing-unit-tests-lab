631f87bb014bdab2454336e9410f8863ce05aeae
IsaiahMitchell <zaymgmt707@gmail.com>
2025-05-03 11:50:58 -0700

    updates import path utils.test.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,25 @@
-// Your tests here
+import { isPalindrome } from '../utils.js'
+
+test('racecar is a palindrome', () => {
+  expect(isPalindrome('racecar')).toBe(true)
+})
+
+test('car is not a palindrome', () => {
+  expect(isPalindrome('car')).toBe(false)
+})
+
+test('handles uppercase letters', () => {
+  expect(isPalindrome('Mom')).toBe(true)
+})
+
+test('empty string is false', () => {
+  expect(isPalindrome('')).toBe(false)
+})
+
+test('throws on numbers', () => {
+  expect(() => isPalindrome('123')).toThrow()
+})
+
+test('throws on non-string input', () => {
+  expect(() => isPalindrome(123)).toThrow()
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
-// Your code here
+export function isPalindrome(word) {
+  if (typeof word !== 'string') throw Error('Must be a string')
+  if (word === '') return false
+  if (!/^[A-Za-z]+$/.test(word)) throw Error('Letters only')
+
+  word = word.toLowerCase()
+  return word === word
+    .split('')
+    .reverse()
+    .join('')
+}
+
